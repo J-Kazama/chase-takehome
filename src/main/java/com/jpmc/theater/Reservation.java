f36103dd7cfb990db312799733c23f6dd1389e39
J-Kazama <davidoy123@gmail.com>
2022-10-08 16:07:26 -0400

    removed audience count from reservation logic and fixed fee calculation

--- src/main/java/com/jpmc/theater/Reservation.java
+++ src/main/java/com/jpmc/theater/Reservation.java
@@ -3,15 +3,13 @@ package com.jpmc.theater;
 public class Reservation {
     private Customer customer;
     private Showing showing;
-    private int audienceCount;
 
-    public Reservation(Customer customer, Showing showing, int audienceCount) {
+    public Reservation(Customer customer, Showing showing) {
         this.customer = customer;
         this.showing = showing;
-        this.audienceCount = audienceCount;
     }
 
     public double totalFee() {
-        return showing.getMovieFee() * audienceCount;
+        return showing.getMovieFee();
     }
 }
