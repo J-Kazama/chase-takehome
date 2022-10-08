336a96f5902a97b95e684b9a87a3a0e3513a4e13
J-Kazama <davidoy123@gmail.com>
2022-10-08 16:26:34 -0400

    getter method for reservation

--- src/main/java/com/jpmc/theater/Reservation.java
+++ src/main/java/com/jpmc/theater/Reservation.java
@@ -3,13 +3,19 @@ package com.jpmc.theater;
 public class Reservation {
     private Customer customer;
     private Showing showing;
+    private int ticketCount;
 
-    public Reservation(Customer customer, Showing showing) {
+    public Reservation(Customer customer, Showing showing, int ticketCount) {
         this.customer = customer;
         this.showing = showing;
+        this.ticketCount = ticketCount;
     }
 
     public double totalFee() {
-        return showing.calculateFee();
+        return showing.calculateFee() * this.ticketCount;
+    }
+
+    public Customer getCustomer() {
+        return this.customer;
     }
 }
