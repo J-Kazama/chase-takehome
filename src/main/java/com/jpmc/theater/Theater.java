df977e08fa83711cbd0ca47ddcbd58379916784c
J-Kazama <davidoy123@gmail.com>
2022-10-08 15:32:54 -0400

    added utility method to setup schedule

--- src/main/java/com/jpmc/theater/Theater.java
+++ src/main/java/com/jpmc/theater/Theater.java
@@ -12,11 +12,14 @@ public class Theater {
 
     public Theater(LocalDateProvider provider) {
         this.provider = provider;
+        schedule = setupSchedule();
+    }
 
+    public List<Showing> setupSchedule(){
         Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, 1);
         Movie turningRed = new Movie("Turning Red", Duration.ofMinutes(85), 11, 0);
         Movie theBatman = new Movie("The Batman", Duration.ofMinutes(95), 9, 0);
-        schedule = List.of(
+        return List.of(
             new Showing(turningRed, 1, LocalDateTime.of(provider.currentDate(), LocalTime.of(9, 0))),
             new Showing(spiderMan, 2, LocalDateTime.of(provider.currentDate(), LocalTime.of(11, 0))),
             new Showing(theBatman, 3, LocalDateTime.of(provider.currentDate(), LocalTime.of(12, 50))),
