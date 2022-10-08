c5e271786a2480b57c5beb83ce32cf907c38e13b
J-Kazama <davidoy123@gmail.com>
2022-10-08 15:30:19 -0400

    adjust variable name after typo fix

--- src/main/java/com/jpmc/theater/Theater.java
+++ src/main/java/com/jpmc/theater/Theater.java
@@ -19,13 +19,13 @@ public class Theater {
         schedule = List.of(
             new Showing(turningRed, 1, LocalDateTime.of(provider.currentDate(), LocalTime.of(9, 0))),
             new Showing(spiderMan, 2, LocalDateTime.of(provider.currentDate(), LocalTime.of(11, 0))),
-            new Showing(theBatMan, 3, LocalDateTime.of(provider.currentDate(), LocalTime.of(12, 50))),
+            new Showing(theBatman, 3, LocalDateTime.of(provider.currentDate(), LocalTime.of(12, 50))),
             new Showing(turningRed, 4, LocalDateTime.of(provider.currentDate(), LocalTime.of(14, 30))),
             new Showing(spiderMan, 5, LocalDateTime.of(provider.currentDate(), LocalTime.of(16, 10))),
-            new Showing(theBatMan, 6, LocalDateTime.of(provider.currentDate(), LocalTime.of(17, 50))),
+            new Showing(theBatman, 6, LocalDateTime.of(provider.currentDate(), LocalTime.of(17, 50))),
             new Showing(turningRed, 7, LocalDateTime.of(provider.currentDate(), LocalTime.of(19, 30))),
             new Showing(spiderMan, 8, LocalDateTime.of(provider.currentDate(), LocalTime.of(21, 10))),
-            new Showing(theBatMan, 9, LocalDateTime.of(provider.currentDate(), LocalTime.of(23, 0)))
+            new Showing(theBatman, 9, LocalDateTime.of(provider.currentDate(), LocalTime.of(23, 0)))
         );
     }
 
