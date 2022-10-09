7ae80282d03288f4ed1eaf0bafb46a853555463e
J-Kazama <davidoy123@gmail.com>
2022-10-09 09:24:17 -0400

    unit tests and comments for movie class

--- src/main/java/com/jpmc/theater/Movie.java
+++ src/main/java/com/jpmc/theater/Movie.java
@@ -8,11 +8,18 @@ public class Movie {
     private static int MOVIE_CODE_SPECIAL = 1;
 
     private String title;
-    private String description;
     private Duration runningTime;
     private double ticketPrice;
     private int specialCode;
 
+    /** Constructor for the movie class. A movie is composed of a title, length (running time), ticket price
+     * and a code for whether it is a special movie or not (related to a special discount)
+     * 
+     * @param title       title of the movie
+     * @param runningTime the length of the movie
+     * @param ticketPrice the price for a single ticket for this movie
+     * @param specialCode the code for this movie
+     */
     public Movie(String title, Duration runningTime, double ticketPrice, int specialCode) {
         this.title = title;
         this.runningTime = runningTime;
@@ -20,29 +27,56 @@ public class Movie {
         this.specialCode = specialCode;
     }
 
+    /** Title getter.
+     * 
+     * @return the title of the movie
+     */
     public String getTitle() {
         return title;
     }
 
+    /** Running time getter.
+     * 
+     * @return the length of the movie
+     */
     public Duration getRunningTime() {
         return runningTime;
     }
 
+    /** Ticket price getter.
+     * 
+     * @return the price for a single ticket of the movie
+     */
     public double getTicketPrice() {
         return ticketPrice;
     }
 
+    /** Calculates the price for a showing of this movie. The price of the showing would be the
+     * price of the ticket and substracting any discount associated with that specific showing.
+     * 
+     * @param showing the showing of this movie
+     * @return        the price of this showing
+     */
     public double calculateTicketPrice(Showing showing) {
         return ticketPrice - getDiscount(showing.getSequenceOfTheDay(), showing.getStartTime());
     }
 
+    /** Calculates the discount associated with a specific showing. Discount can be applied 
+     * if the movie is a special one (per code), time of showing's start time, and if the movie
+     * is of a sequence that has a discount associated with it. If multiple discounts may be applied
+     * the largest one is applied. 
+     * 
+     * @param showSequence  the number of showing of the current date in the theater
+     * @param showStartTime the time when the showing begins
+     * @return              the amount of the discount
+     */
     private double getDiscount(int showSequence, LocalDateTime showStartTime) {
         double specialDiscount = 0; 
         double midDayDiscount = 0;
         if (MOVIE_CODE_SPECIAL == specialCode) {
             specialDiscount = ticketPrice * 0.2;  // 20% discount for special movie
         }
-        // time is between 11 am to 4 pm
+        // checking if time is between 11 am to 4 pm
         if(showStartTime.getHour() >= 11 && showStartTime.getHour() <= 16) {
             midDayDiscount = ticketPrice * 0.25; // 25% discount for movies starting between 11 am to 4 pm
         }
@@ -58,9 +92,15 @@ public class Movie {
 
         // biggest discount wins
         double percentageDiscount = Math.max(specialDiscount, midDayDiscount);
-        return percentageDiscount > sequenceDiscount ? specialDiscount : sequenceDiscount;
+        return percentageDiscount > sequenceDiscount ? percentageDiscount : sequenceDiscount;
     }
 
+    /** Overriding equality method to check for the fields of the movie for movie equality
+     * when calling movie1.equals(movie2).
+     *  
+     * @param o object passed
+     * @return  true or false depending of if fields match
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -68,11 +108,14 @@ public class Movie {
         Movie movie = (Movie) o;
         return Double.compare(movie.ticketPrice, ticketPrice) == 0
                 && Objects.equals(title, movie.title)
-                && Objects.equals(description, movie.description)
                 && Objects.equals(runningTime, movie.runningTime)
                 && Objects.equals(specialCode, movie.specialCode);
     }
 
+    /** Overriding how a movie object is hashed.
+     * 
+     * @return the hashcode for the movie
+     */
     @Override
     public int hashCode() {
         return Objects.hash(title, description, runningTime, ticketPrice, specialCode);
--- src/main/java/com/jpmc/theater/MovieTests.java
+++ src/main/java/com/jpmc/theater/MovieTests.java
@@ -10,12 +10,67 @@ import java.time.LocalTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovieTests {
+    
+    /** Tests for no discount as the movie does not have a sequence that has
+     * a discount associated with it, nor the special movie code set or a mid
+     * day view time
+     */
     @Test
-    void specialMovieWith50PercentDiscount() {
-        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 1);
-        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
+    void testRegularMovieNoDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 0);
+        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(17, 50)));
         assertEquals(10, spiderMan.calculateTicketPrice(showing));
+    }
+    
+    /** Tests for discount applied for mid day view time. */
+    @Test
+    void testRegularMovieMidDayDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 0);
+        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 50)));
+        assertEquals(7.5, spiderMan.calculateTicketPrice(showing));
+    }
+    
+    /** Tests for sequence disccount applied. */
+    @Test
+    void testRegularMovieSequenceDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 0);
+        Showing showing = new Showing(spiderMan, 2, LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 50)));
+        assertEquals(8, spiderMan.calculateTicketPrice(showing));
+    }
 
-        System.out.println(Duration.ofMinutes(90));
+    /** Tests for special movie discount. */
+    @Test
+    void testSpecialMovieDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 50)));
+        assertEquals(8, spiderMan.calculateTicketPrice(showing));
+    }
+
+    /** Tests for largest discount applied. For a special movie, that is viewing middle of the day, and that 
+     * has sequence of 1, the discount should be largest value out of {moviePrice * 0.2, moviePrice * 0.25, 3}
+     */
+    @Test
+    void testLargestDiscountApplied() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Showing showing = new Showing(spiderMan, 1, LocalDateTime.of(LocalDate.now(), LocalTime.of(13, 50)));
+        assertEquals(7, spiderMan.calculateTicketPrice(showing));
+    }
+
+    /** Tests for movie equality given all fields are identical. */
+    @Test
+    void testMoviesIdentical() {
+        Movie spiderManMovie = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Movie identicalSpiderMovie = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1); 
+        boolean isEqual = spiderManMovie.equals(identicalSpiderMovie);
+        assertTrue(isEqual);
+    }
+
+    /** Tests for movie equality given difference in a field. */
+    @Test
+    void testMoviesNotIdentical() {
+        Movie spiderManMovie = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),10, 1);
+        Movie nearlyIdenticalSpiderMovie = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),11, 1); 
+        boolean isEqual = spiderManMovie.equals(nearlyIdenticalSpiderMovie);
+        assertFalse(isEqual);
     }
 }
